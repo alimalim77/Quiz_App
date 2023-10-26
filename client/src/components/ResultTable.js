18db52c5d42daa965d14d0c2027aa14e74726bcc
alimalim77 <alimalim77@gmail.com>
2023-10-26 17:33:34 +0530

    Commit: Check Array

--- client/src/components/ResultTable.js
+++ client/src/components/ResultTable.js
@@ -1,41 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { getServerData } from "../helper/helper";
-
-// export default function ResultTable() {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     getServerData(`${process.env.PORT}/api/result`, (res) => {
-//       setData(res);
-//     });
-//   });
-
-//   return (
-//     <div>
-//       <table>
-//         <thead className="table-header">
-//           <tr className="table-row">
-//             <td>Name</td>
-//             <td>Attemps</td>
-//             <td>Earn Points</td>
-//             <td>Result</td>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {!data ?? <div>No Data Found </div>}
-//           {data.map((v, i) => (
-//             <tr className="table-body" key={i}>
-//               <td>{v?.username || ""}</td>
-//               <td>{v?.attempts || 0}</td>
-//               <td>{v?.points || 0}</td>
-//               <td>{v?.achived || ""}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
 import React, { useEffect, useState } from "react";
 import { getServerData } from "../helper/helper";
 
@@ -43,10 +5,12 @@ export default function ResultTable() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Use the correct environment variable, process.env.PORT is not typically used for API endpoints
+    // Use the correct environment variable, process.env.REACT_APP_PORT is typically used for React apps
     getServerData(`${process.env.REACT_APP_PORT}/api/result`, (res) => {
-      setData(res);
-      console.log(data);
+      // Ensure that res is an array before setting it in the state
+      if (Array.isArray(res)) {
+        setData(res);
+      }
     });
   }, []); // Empty dependency array to run the effect only once
 
@@ -61,7 +25,7 @@ export default function ResultTable() {
             <td>Result</td>
           </tr>
         </thead>
-        {/* <tbody>
+        <tbody>
           {data.length === 0 ? (
             <tr>
               <td colSpan="4">No Data Found</td>
@@ -76,7 +40,7 @@ export default function ResultTable() {
               </tr>
             ))
           )}
-        </tbody> */}
+        </tbody>
       </table>
     </div>
   );
